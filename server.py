43421d0d1e8b2a4b412b1672d8176cdcc511a7da
Amritanshu Lahiri <116184733+Deadshot1611@users.noreply.github.com>
2025-06-26 04:55:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -370,7 +370,7 @@ async def main():
     port = int(os.environ.get("PORT", 8085))
     
     await mcp.run_async(
-        "streamable-http",
+        "http",
         host="0.0.0.0",
         port=port,
     )
